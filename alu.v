20908d978f8d179208b8f1a7c78a2759135fcfd5
Nathaniel Brooke <neb17@duke.edu>
2018-03-30 00:04:39 -0400

    Removed slow modules from processor

--- alu.v
+++ alu.v
@@ -15,11 +15,11 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 		.ovf(ovfadd), .sum(add_out), .cout(un));
 	sub_comp subtractor(.inA(data_operandA), .inB(data_operandB), .lt(isLessThan), 
 		.neq(isNotEqual), .ovf(ovfsub), .sum(sub_out));
-	bitwise_and and_module(.inA(data_operandA), .inB(data_operandB), .out(and_out));
-	bitwise_or or_module(.inA(data_operandA), .inB(data_operandB), .out(or_out));
-	sll left_shift(.in(data_operandA), .shamt(ctrl_shiftamt), .out(sll_out));
-	sra right_shift(.in(data_operandA), .shamt(ctrl_shiftamt), .out(sra_out));
-	
+	assign and_out = data_operandA & data_operandB;
+	assign or_out = data_operandA | daba_operandB;
+	assign sll_out = data_operandA << ctrl_shiftamt;
+	assign sra_out = data_operandA >>> ctrl_shiftamt;
+		
 	assign overflow = ctrl_ALUopcode[0] ? ovfsub : ovfadd;
 	tristate_8 outmux(.sel(ctrl_ALUopcode[2:0]), .in0(add_out), .in1(sub_out), .in2(and_out), .in3(or_out), 
 							.in4(sll_out), .in5(sra_out), .in6(32'b0), .in7(32'b0), .out(data_result));
