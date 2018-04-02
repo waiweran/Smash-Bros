3fe2799d713b0ee20d4a787861d56e1c78282fda
Nathaniel Brooke <neb17@duke.edu>
2018-04-02 15:26:53 -0400

    Updated physics coprocessor to take in controls and output position

--- physics_coprocessor.v
+++ physics_coprocessor.v
@@ -1,95 +1,54 @@
 module physics_coprocessor(
-    // Control signals
-    clock,                          // I: The master clock
-    reset,                          // I: A reset signal
-    call,									// I: Call to the coprocessor
-	 
-	 mass1, mass2,							// I: Player masses
-	 startPos1, startPos2,				// I: Starting positions
-	 
-	 force1, force2,						// I: Player Force Inputs
-	 freeze1, freeze2,					// I: Player Freeze Inputs
-	 paralyze1, paralyze2,				// I: Player paralyze inputs
-	 
-	 wallL1, wallL2,						// I: Player has wall on left
-	 wallR1, wallR2,						// I: Player has wall on right
-	 wallD1, wallD2, 						// I: Player on top of wall (ground)
-	 wallU1, wallU2,						// I: Player has wall above
-	 
-	 platfD1, platfD2,					// I: Player on top of platform
-	 platfT1, platfT2,					// I: Player dropping thru platform
-	 
-	 pos1, pos2								// O: Player positions
-);
-
-    // Control signals
-    input clock, reset, call;
-	 
-	 // Settings
-	 input [31:0] mass1, mass2;
-	 input [63:0] startPos1, startPos2;
-
-    // Input to Engine
-    input [63:0] force1, force2;
-	 input freeze1, freeze2, paralyze1, paralyze2;
-	 
-	 // Output from Engine
-	 output [63:0] pos1, pos2;
-
-    // Players
-    player_physics player1(.clock(clock), .reset(reset), 
-		.mass(mass1), .gravity(32'd10), .wind(32'd1), .start_Position(startPos1), 
-		.wall_Left(wallL1), .wall_Right(wallR1), .wall_Up(wallU1), .wall_Down(wallD1),
-		.platform_Down(platfD1), .platform_Thru(platfT1),
-		.force_in(), .freeze_in(), .paralyze_in(), .paralyze_const(32'd1000000), .position(pos1));
-		
-    player_physics player2(.clock(clock), .reset(reset), 
-		.mass(mass2), .gravity(32'd10), .wind(32'd1), .start_Position(startPos2), 
-		.wall_Left(wallL2), .wall_Right(wallR2), .wall_Up(wallU2), .wall_Down(wallD2),
-		.platform_Down(platfD2), .platform_Thru(platfT2),
-		.force_in(), .freeze_in(), .paralyze_in(), .paralyze_const(32'd1000000), .position(pos2));
-
-    always@(posedge call) begin
-
-    end
-
-endmodule // physics_coprocessor
-
-module player_physics(
 	clock, reset, 			// Master clock, reset signals
 
-	mass, gravity, wind, // Constants set for the player
+	mass, gravity, wind, 	// Constants set for the player
+	start_Position, 		// Starting position [x, y]
 
-	start_Position, 		// Starting position
-
-	force_in, 				// Input force
+	controller_in, 			// Input from controller joystick
 
 	wall_Left, wall_Right, 	// Hitting walls (left, right)
 	wall_Up, wall_Down,		// Hitting walls (up, down)
 	platform_Down,			// Landing on platform
-	platform_Thru, 			// Going through platform
 
 	freeze_in,				// Holds player still
 	paralyze_in,			// Vibrates player
-	paralyze_const, 		// Sets amplitude of vibration
 
-	position 				// Output position
+	position 				// Output position [x, y]
 );
 
 	// Inputs
 	input clock, reset;
 	input [31:0] mass, gravity, wind;
-	input [63:0] start_Position, force_in;
-	input wall_Left, wall_Right, wall_Up, wall_Down, platform_Down, platform_Thru;
+	input [31:0] start_Position;
+	input [31:0] controller_in;
+	input wall_Left, wall_Right, wall_Up, wall_Down, platform_Down;
 	input freeze_in, paralyze_in;
-	input [31:0] paralyze_const;
 
 	// Output Position
-	output [63:0] position;
+	output [31:0] position;
 
-	// X, Y components
+	// Input from Controller
+	wire [7:0] joystick_x, joystick_y; // Unsigned values from 0 to 255 representing joystick position
+	// TODO assign values from controller
+	wire jump;
+	wire platform_Thru;
+	assign jump = = joytcick_y[7] & joystick_y[6] & joystick_y[5] & joystick_y[4]; // Joystick Y 240 to 255 (up)
+	assign platform_Thru = ~joytcick_y[7] & ~joystick_y[6] & ~joystick_y[5] & ~joystick_y[4]; // Joystick Y 0 to 15 (down)
+
+	// Input Physics Parameters
+	wire [31:0] force_x, force_y;
+	assign force_x[7:0] = joystick_x - 8'b10000000; // Map joystick values to -128 to 127
+	assign force_y[7:0] = joystick_y - 8'b10000000; // Map joystick values to -128 to 127
+	genvar i;
+	generate // Extend joystick value to 32 bit signed values
+		for(i = 8; i < 32; i = i + 1) begin: signextend
+			assign force_x[i] = ~joystick_x[8];
+			assign force_y[i] = ~joystick_y[8];
+		end
+	endgenerate
+
+	// X, Y position components
     reg [31:0] pos_x, pos_y;
-    wire [31:0] force_x, force_y;
 
     // Stored Values
     reg [31:0] vel_x, vel_y;
@@ -98,16 +57,33 @@ module player_physics(
     // Vibration Values
     reg [31:0] vibr_pos_y;
     wire vibr_dir;
-    assign vibr_dir = (pos_y < vibr_pos_y + paralyze_const)? 1'b1 : 1'b0;
+    assign vibr_dir = (pos_y < vibr_pos_y + 32'd10000000)? 1'b1 : 1'b0;
 
     // Separate input, output components
-    assign force_x = force_in[63:32];
-    assign force_y =  force_in[31:0];
-    assign position[63:32] = pos_x;
-    assign position[31:0] = pos_y;
+    assign position[31:16] = pos_x[31:16];
+    assign position[15:0] = pos_y[31:16];
 
     // Update values every cycle
     always@(posedge clock) begin
+
+    	// Acceleration, velocity update for in air
+    	if(~freeze_in & ~paralyze_in & ~wall_Down) begin
+    		accel_x <= force_x / mass - vel_x * vel_x * vel_x / wind;
+    		accel_y <= gravity - force_y / mass + vel_y * vel_y * vel_y / wind;
+    		vel_x <= vel_x + accel_x;
+    		vel_y <= vel_y  + accel_y;
+    		pos_x <= pos_x + vel_x;
+    		pos_y <= pos_y + vel_y;
+    	end
+
+    	// Acceleration, velocity update for on ground
+    	if(~freeze_in & ~paralyze_in & wall_Down) begin
+    		accel_x <= 32'b0;
+    		accel_y <= 32'b0;
+    		vel_x <= force_x / mass;
+    		vel_y <= 32'b0;
+    		pos_x <= pos_x + vel_x;
+    	end
 
     	// Wall hitting calculations
     	if(wall_Left & vel_x < 0) begin
@@ -128,36 +104,26 @@ module player_physics(
     		vel_y <= 0;
     	end
 
-    	// Acceleration, velocity update
-    	accel_x <= force_x / mass - vel_x * vel_x * vel_x / wind;
-    	accel_y <= gravity - force_y / mass - vel_y * vel_y * vel_y / wind;
-    	vel_x <= vel_x + accel_x;
-    	vel_y <= vel_y  + accel_y;
-
-    	// Paralyzer
+    	// Paralyzer Vibration
     	if(paralyze_in) begin
     		if(vibr_dir)
-    			pos_y <= pos_y + 31'd100;
+    			pos_y <= pos_y + 32'd2;
     		else
-    			pos_y <= pos_y - 31'd100; 
-    	end
-
-    	// Freeze
-    	else if(~freeze_in | ~paralyze_in) begin
-    		pos_x <= pos_x + vel_x;
-    		pos_y <= pos_y + vel_y;
+    			pos_y <= pos_y - 32'd2; 
     	end
 
     end
 
     // Reset values
     always@(posedge reset) begin
-    	accel_x <= 0;
-    	accel_y <= 0;
-    	vel_x <= 0;
-    	vel_y <= 0;
-    	pos_x <= start_Position[63:32];
-    	pos_y <= start_Position[31:0];
+    	accel_x <= 32'b0;
+    	accel_y <= 32'b0;
+    	vel_x <= 32'b0;
+    	vel_y <= 32'b0;
+    	pos_x [31:16] <= start_Position[31:16];
+    	pos_x [15:0] <= 16'b0;
+    	pos_y [31:16] <= start_Position[15:0];
+    	pos_y [15:0] <= 16'b0;
     end
 
     // Store paralyzer position
