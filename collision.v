5e83b3305af2ab88c7392a0380705c96aa93219d
Jesse Yue <someonebob156@gmail.com>
2018-04-08 01:08:28 -0400

    fix collisions

--- collision.v
+++ collision.v
@@ -10,8 +10,19 @@ module collision(
 	
 	// pos[31:16] x, pos[15:0] y, size[31:16] width, size[15:0] height
 
-	assign coll[3] = ~(x_pos[31:16] > (y_pos[31:16]+y_size[31:16])); 	// left collision
-	assign coll[2] = ~((x_pos[31:16]+x_size[31:16]) < y_pos[31:16]); 	// right collision
-	assign coll[1] = ~(x_pos[15:0] > (y_pos[15:0]+y_size[15:0])); 		// bottom collision
-	assign coll[0] = ~((x_pos[15:0]+x_size[15:0]) < y_pos[15:0]);		// top collision
+	assign coll[3] = ~(x_pos[31:16] > (y_pos[31:16]+y_size[31:16]))	// left collision 
+							& (x_pos[15:0] < (y_pos[15:0]+y_size[15:0]))		// bottom below top edge of stage
+							& ((x_pos[15:0]+x_size[15:0]) > y_pos[15:0]);	// top above bottom of stage
+							
+	assign coll[2] = ~((x_pos[31:16]+x_size[31:16]) < y_pos[31:16]) 	// right collision
+							& (x_pos[15:0] < (y_pos[15:0]+y_size[15:0]))		// bottom below top edge of stage
+							& ((x_pos[15:0]+x_size[15:0]) > y_pos[15:0]);	// top above bottom of stage
+							
+	assign coll[1] = ~(x_pos[15:0] > (y_pos[15:0]+y_size[15:0])) 		// bottom collision
+							& (x_pos[31:16] < (y_pos[31:16]+y_size[31:16]))	// left edge within right edge of stage
+							& ((x_pos[31:16]+x_size[31:16]) > y_pos[31:16]);// right edge within left edge of stage
+	
+	assign coll[0] = ~((x_pos[15:0]+x_size[15:0]) < y_pos[15:0])		// top collision
+							& (x_pos[31:16] < (y_pos[31:16]+y_size[31:16]))	// left edge within right edge of stage
+							& ((x_pos[31:16]+x_size[31:16]) > y_pos[31:16]);// right edge within left edge of stage
 endmodule
